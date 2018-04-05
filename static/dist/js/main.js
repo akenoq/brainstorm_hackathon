14c75bc797cd3a314124b76b266bbf92eae2a91d
akenoq <akenoq@yandex.ru>
2018-04-05 16:24:57 +0300

    add github link

---
+++ static/dist/js/main.js
@@ -0,0 +1,5 @@
+window.addEventListener("load", function () {
+    document.querySelector("header").hidden = false;
+    document.querySelector("#team-carousel").hidden = false;
+    document.querySelector(".footer").hidden = false;
+});
